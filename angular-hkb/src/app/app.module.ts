c5362b9ddab10ea7cc28c499e1e97bd7f0560267
hkb <hkb@tisynch.com>
2020-09-13 20:51:39 +0200

    add material features

--- angular-hkb/src/app/app.module.ts
+++ angular-hkb/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './modules/about/about.component';
 import { APP_ROUTES } from './app.routes';
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
+    MatSliderModule,
+    RouterModule.forRoot(APP_ROUTES),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-hkb/src/app/app.routes.ts
+++ angular-hkb/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './modules/about/about.component';
+import { ProjectsComponent } from './modules/projects/projects.component';
 
 export const APP_ROUTES: Routes = [
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent }
 ];
