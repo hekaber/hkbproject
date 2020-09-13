e8d1702466b5ae44005635804ed951464db3ae07
hkb <hkb@tisynch.com>
2020-09-13 22:00:54 +0200

    header component

--- angular-hkb/src/app/app.module.ts
+++ angular-hkb/src/app/app.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './modules/about/about.component';
 import { APP_ROUTES } from './app.routes';
 import { ProjectsComponent } from './modules/projects/projects.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './modules/navigation/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
