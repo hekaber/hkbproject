f2e49f5c1cc0e5f2d7b866bde860f1245f581c10
hkb <hkb@tisynch.com>
2020-09-26 14:03:52 +0200

    implement side menu

--- angular-hkb/src/app/app.component.ts
+++ angular-hkb/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-
-}
+export class AppComponent {}
