16417859559244276c7ecb66e41de26d8546fac4
hkb <hkb@tisynch.com>
2020-09-27 15:54:26 +0200

    animating logos

--- angular-hkb/src/app/components/home/home.component.ts
+++ angular-hkb/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Observable } from 'rxjs';
 
 import { JobService } from '../../shared/providers/job.service';
@@ -7,12 +8,26 @@ import { Job } from '../../shared/models/job';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('changeDivOpacity', [
+      state('initial', style({
+        opacity: 0
+      })),
+      state('final', style({
+        opacity: 1
+      })),
+      transition('initial=>final', animate('2000ms')),
+      transition('final=>initial', animate('1000ms'))
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
 
   jobs$: Observable<Job[]>;
-
+  currentState = 'initial';
+  logos = ['python.svg', 'php.svg', 'java.svg', 'javascript.svg', 'css.svg', 'html.svg'];
+  
   constructor(private service: JobService) { }
 
   ngOnInit(): void {
@@ -26,5 +41,6 @@ export class HomeComponent implements OnInit {
       block: "start", 
       inline: "nearest"
     });
+    this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
   }
 }
